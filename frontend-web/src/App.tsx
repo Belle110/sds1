eaa5e9496e3520fa22c374255cae6437dd2376da
isabelle <isa_belle.belinha@hotmail.com>
2020-09-17 23:27:35 -0300

    Adding header component

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <h1>Hello semana Dev Superior!</h1>  
+    <div>
+      <Header />
+    </div>
   );
 }
 
