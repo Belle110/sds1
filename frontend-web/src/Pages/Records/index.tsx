e29b810c5f35456942a41d988a71d57379dabda0
isabelle <isa_belle.belinha@hotmail.com>
2020-09-20 11:43:44 -0300

    Configuring frontend-web for production

--- frontend-web/src/Pages/Records/index.tsx
+++ frontend-web/src/Pages/Records/index.tsx
@@ -7,7 +7,7 @@ import Pagination from './Pagination';
 import Filters from '../../Components/Filters';
 
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-isabelle.herokuapp.com';
 
 const Records = () => {
     const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
